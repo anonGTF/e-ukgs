539c7f56b5af1d781e3fbec122c1de51c211faef
galih.tegar <galih.tegar.febrianto@bni.co.id>
2025-03-02 10:51:43 +0700

    feat(activity: add, budgeting): merge add and budgeting into single page

--- pages/teacher/activity/add.vue
+++ pages/teacher/activity/add.vue
@@ -64,15 +64,91 @@
                 @change="data => selectedPic = data.data"
                 class="w-full"
             />
-            <Spacer height="h-12" />
-            <Button 
-                full-width
-                :loading="isLoading"
-                @click="create"
-            >
-                Buat Kegiatan
-            </Button>
+            <Spacer class="h-6"/>
+            <Toggle
+                v-model="isNeedBudget"
+                label="Apakah kegiatan ini membutuhkan biaya?"
+            />
         </div>
+        <template v-if="isNeedBudget">
+            <Spacer class="h-6"/>
+            <div class="bg-white border border-border-primary rounded-2xl p-6">
+                <DataTable :headers="tableHeader">
+                    <tr v-for="(data, index) in budgetingItems">
+                        <th>
+                            <Text :typography="Typography.Body2" class="font-semibold text-content-primary">{{ index + 1 }}</Text>
+                        </th>
+                        <td>
+                            <TextField 
+                                v-model="data.name"
+                                placeholder="Nama alat/bahan"
+                            />
+                        </td>
+                        <td>
+                            <TextField 
+                                v-model="data.unit"
+                                placeholder="Satuan"
+                            />
+                        </td>
+                        <td>
+                            <TextField 
+                                v-model:number="data.count"
+                                type="number"
+                                placeholder="Jumlah"
+                            />
+                        </td>
+                        <td>
+                            <AmountTextField
+                                v-model="data.pricePerUnit"
+                                placeholder="Harga satuan (Rp)"
+                            />
+                        </td>
+                        <td>
+                            <Text class="font-semibold">Rp{{ formatPrice(data.subtotal) }}</Text>
+                        </td>
+                        <td class="flex justify-end gap-2">
+                            <Button 
+                                v-if="budgetingItems.length > 1"
+                                :type="ButtonType.Primary" 
+                                class="bg-error border-none hover:bg-error/75"
+                                @click="deleteItem(index)"
+                            >
+                                <Icon name="mdi:trash-can" class="text-white"/>
+                            </Button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="5">
+                            <Text :typography="Typography.Label" color="text-black" class="text-end font-semibold">TOTAL</Text>
+                        </td>
+                        <td colspan="2">
+                            <Text :typography="Typography.Label" color="text-black" class="font-semibold">Rp{{ formatPrice(budgetTotal) }}</Text>
+                        </td>
+                    </tr>
+                </DataTable>
+                <Spacer height="h-4"/>
+                <Button 
+                    full-width
+                    :type="ButtonType.DashOutlined"
+                    class="hover:bg-gray-200 hover:text-black hover:border-gray-200"
+                    @click="addItem"
+                >
+                    <span class="flex flex-row items-center">
+                        <Icon name="mdi:plus"/>
+                        <Spacer width="w-2"/>
+                        Tambah Item
+                    </span>
+                </Button>
+            </div>
+        </template>
+        <Spacer height="h-6" />
+        <Button 
+            full-width
+            :loading="isLoading"
+            @click="create"
+        >
+            Buat Kegiatan
+        </Button>
     </div>
 </template>
 
@@ -144,6 +220,49 @@
     } satisfies CustomDropdownOption<User>)) ?? [])
     const selectedPic = ref<User | null>(null)
     const wrapPicWithDropdownOption = (data: User | null) => picOptions.value?.find((option) => option.data == data)
+    const isNeedBudget = ref(true)
+
+    const tableHeader = ref([
+        "",
+        "Nama Alat/Bahan",
+        "Satuan",
+        "Jumlah",
+        "Harga Satuan (Rp)",
+        "Subtotal (Rp)",
+        ""
+    ])
+
+    const budgetingItems = ref<Budget[]>([
+        {
+            name: "",
+            unit: "",
+            count: 0,
+            pricePerUnit: 0,
+            subtotal: 0
+        }
+    ])
+
+    watch(
+        budgetingItems,
+        (items) => items.forEach((item) => item.subtotal = item.count * item.pricePerUnit),
+        { deep: true }
+    )
+
+    const addItem = () => {
+        budgetingItems.value.push({
+            name: "",
+            unit: "",
+            count: 0,
+            pricePerUnit: 0,
+            subtotal: 0
+        })
+    }
+
+    const deleteItem = (index: number) => {
+        budgetingItems.value.splice(index, 1)
+    }
+
+    const budgetTotal = computed(() => budgetingItems.value.reduce(((acc, curr) => acc + curr.subtotal), 0))
 
     const isLoading = ref(false)
 
@@ -170,8 +289,37 @@
         if (isLeft(result)) {
             uiStore.showToast(unwrapEither(result), ToastType.ERROR)
         } else {
-            uiStore.showToast("Berhasil menambahkan kegiatan", ToastType.SUCCESS)
-            router.go(-1)
+            await saveBudgeting(unwrapEither(result))
         }
+    }
+
+    const saveBudgeting = async (activityId: string) => {
+        const result = await useAddBudgetPlan(
+            userStore.school?.id ?? "", 
+            activityId,
+            {
+                id: UPDATE_CONSTANTS.budgetPlanAttr,
+                items: isNeedBudget.value ? budgetingItems.value : [],
+                total: isNeedBudget.value ? budgetTotal.value : 0
+            }
+        )
+
+        if (isLeft(result)) {
+            uiStore.showToast(unwrapEither(result), ToastType.ERROR)
+            router.back()
+            return
+        }
+
+        const activityUpdateResult = await useUpdateActivityStatus(userStore.school?.id ?? "", activityId, ActivityStatus.READY)
+
+        if (isLeft(activityUpdateResult)) {
+            uiStore.showToast(unwrapEither(activityUpdateResult), ToastType.ERROR)
+            router.back()
+            return
+        }
+
+        uiStore.showToast("Berhasil menambahkan kegiatan", ToastType.SUCCESS)
+        router.go(-1)
+        return
     }
 </script>
